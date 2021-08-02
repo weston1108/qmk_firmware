d6f14a22171e161e8dbfd7a26bbed2c172970d68
Weston Preecs <westonpreecs1108@gmail.com>
2021-08-02 11:01:41 -0700

    Add files via upload

--- keyboards/clawsome/sidekick/keymaps/weston1108/keymap.c
+++ keyboards/clawsome/sidekick/keymaps/weston1108/keymap.c
@@ -13,9 +13,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/* test comment */
 #include QMK_KEYBOARD_H
 enum custom_keycodes {
-	CMD_01,
+	CMD_01 = SAFE_RANGE,
 	CMD_02,
 	CMD_03,
 	CMD_04,
@@ -35,119 +37,119 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	case CMD_01:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 01-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_02:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 02-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_03:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 03-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_04:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 04-"SS_TAP(X_TAB));
-		} 
-		else {
-			
 		}
-		break;		
+		else {
+
+		}
+		break;
 
 	case CMD_05:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 05-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_06:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 06-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_07:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 07-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_08:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 08-"SS_TAP(X_TAB));
-		} 
-		else {
-			
 		}
-		break;	
-	
+		else {
+
+		}
+		break;
+
 	case CMD_09:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 09-"SS_TAP(X_TAB));
-		} 
-		else {
-			
 		}
-		break;		
+		else {
+
+		}
+		break;
 
 	case CMD_10:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 10-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_11:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 11-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_12:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 12-"SS_TAP(X_TAB));
-		} 
+		}
 		else {
-			
+
 		}
 		break;
-	
+
 	case CMD_13:
 		if (record->event.pressed) {
 			SEND_STRING("Cmd /c 13-"SS_TAP(X_TAB));
-		} 
-		else {
-			
 		}
-		break;	
+		else {
+
+		}
+		break;
 	}
 	return true;
 };
