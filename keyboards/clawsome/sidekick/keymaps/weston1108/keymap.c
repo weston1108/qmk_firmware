4e10cbbb2092684faf8714cddb02f47d3738486a
Weston Preecs <westonpreecs1108@gmail.com>
2021-08-06 10:33:40 -0700

    Add files via upload

--- keyboards/clawsome/sidekick/keymaps/weston1108/keymap.c
+++ keyboards/clawsome/sidekick/keymaps/weston1108/keymap.c
@@ -13,11 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* test comment */
 #include QMK_KEYBOARD_H
+
 enum custom_keycodes {
-	CMD_01 = SAFE_RANGE,
+    CMD_01 = SAFE_RANGE,
 	CMD_02,
 	CMD_03,
 	CMD_04,
@@ -30,13 +29,20 @@ enum custom_keycodes {
 	CMD_11,
 	CMD_12,
 	CMD_13,
+    NET_01,
+    NET_02,
+    PASS_01,
+    PRNT_01,
+    PRNT_02,
+    PRNT_03,
+    PRNT_04
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 	case CMD_01:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 01-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 01-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -45,7 +51,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_02:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 02-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 02-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -54,7 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_03:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 03-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 03-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -63,7 +69,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_04:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 04-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 04-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -72,7 +78,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_05:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 05-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 05-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -81,7 +87,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_06:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 06-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 06-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -90,7 +96,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_07:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 07-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 07-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -99,7 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_08:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 08-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 08-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -108,7 +114,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_09:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 09-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 09-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -117,7 +123,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_10:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 10-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 10-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -126,7 +132,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_11:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 11-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 11-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -135,7 +141,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_12:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 12-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 12-" SS_TAP(X_TAB));
 		}
 		else {
 
@@ -144,7 +150,70 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 	case CMD_13:
 		if (record->event.pressed) {
-			SEND_STRING("Cmd /c 13-"SS_TAP(X_TAB));
+			SEND_STRING("Cmd /c 13-" SS_TAP(X_TAB));
+		}
+		else {
+
+		}
+		break;
+
+	case NET_01:
+		if (record->event.pressed) {
+			SEND_STRING("Net use F: \\\\wpcwds-ba.wpcarey.ad.asu.edu\\DATA /user:asurite\\wpreecs");
+		}
+		else {
+
+		}
+		break;
+
+	case NET_02:
+		if (record->event.pressed) {
+			SEND_STRING("Net use O: \\\\wpcsw.wpcarey.ad.asu.edu\\sw /user:asurite\\wpreecs");
+		}
+		else {
+
+		}
+		break;
+
+	case PASS_01:
+		if (record->event.pressed) {
+			SEND_STRING("NoPassForYou");
+		}
+		else {
+
+		}
+		break;
+
+	case PRNT_01:
+		if (record->event.pressed) {
+			SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(1500) "Credential Man");
+		}
+		else {
+
+		}
+		break;
+
+	case PRNT_02:
+		if (record->event.pressed) {
+			SEND_STRING("\\\\wpcpapercut.wpcarey.ad.asu.edu");
+		}
+		else {
+
+		}
+		break;
+
+	case PRNT_03:
+		if (record->event.pressed) {
+			SEND_STRING("smb://wpcpapercut.wpcarey.ad.asu.edu/WPC_Grad");
+		}
+		else {
+
+		}
+		break;
+
+	case PRNT_04:
+		if (record->event.pressed) {
+			SEND_STRING("asuad\\");
 		}
 		else {
 
@@ -157,24 +226,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /*
     * ------------------------------------
-    * |INS |HOME|PGUP|NUM | /  | *  | -  |
+    * |___ |____|____|____|____|____|____|
     * ------------------------------------
-    * |DEL |END |PGDN| 7  | 8  | 9  | +  |
+    * |___ |___ |____| 7  | 8  | 9  | Tab|
     * ------------------------------|    |
     * |    |    |    | 4  | 5  | 6  |    |
     * ------------------------------------
     * |    | UP |    | 1  | 2  | 3  |ENT |
     * ------------------------------|    |
-    * |LEFT|DOWN|RGHT| 0       | .  |    |
+    * |LEFT|DOWN|RGHT| 0      |_____|    |
     * ------------------------------------
     */
     [0] = LAYOUT(
-        KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_DEL,  KC_END,  KC_PGDN, CMD_07,   CMD_08,   CMD_09,   KC_PPLS,
-                                   CMD_04,   CMD_05,   CMD_06,
-                 KC_UP,            CMD_01,   CMD_02,   CMD_03,   KC_PENT,
-        KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,            KC_PDOT
+        PRNT_01,  PRNT_03, PRNT_04, KC_NLCK, KC_PSLS, KC_BSPC, MO(1),
+        PRNT_02,  KC_END,  KC_PGDN, CMD_07,  CMD_08,  CMD_09,   KC_TAB,
+                                   CMD_04,  CMD_05,  CMD_06,
+                 KC_UP,            CMD_01,  CMD_02,  CMD_03,   KC_PENT,
+        KC_LEFT, KC_DOWN, KC_RGHT, KC_0,            NET_01
     ),
 
+    [1] = LAYOUT(
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, PASS_01, KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+                                   KC_TRNS,  KC_TRNS,  KC_TRNS,
+                 KC_TRNS,            CMD_11,  CMD_12,  CMD_13,   KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, CMD_10,            NET_02
+    ),
 };
 
--- keyboards/clawsome/sidekick/rules.mk
+++ keyboards/clawsome/sidekick/rules.mk
@@ -22,3 +22,4 @@ MIDI_ENABLE = no             # MIDI support
 UNICODE_ENABLE = no          # Unicode
 BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no            # Audio output on port C6
+TAP_DANCE_ENABLE = no	 	 # Tab Dance Enable
